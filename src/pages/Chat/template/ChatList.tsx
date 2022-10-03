29428d56d8293d62d4a7185b33466d5fedad8270
Ken <gialap2002@gmail.com>
2022-10-03 14:21:38 +0700

    feat: add chat list

--- src/pages/Chat/template/ChatList.tsx
+++ src/pages/Chat/template/ChatList.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import SearchIcon from "@mui/icons-material/Search";
-import { Avatar, InputAdornment, TextField } from "@mui/material";
+import {
+  Sidebar,
+  Search,
+  Conversation,
+  ConversationList,
+  Avatar,
+} from "@chatscope/chat-ui-kit-react";
+import checkIfImageExists from "../../../utils/helpers/checkIfImageExists";
+import { DefaultUser } from "../../../assets";
 
 type Props = {
   chatUsers: any[];
@@ -10,43 +17,34 @@ const ChatList = (props: Props) => {
   const { chatUsers } = props;
 
   return (
-    <div className="chat-list">
-      <div className="w-100 p-3">
-        <TextField
-          id="input-search-users"
-          label="Search users"
-          className="chat-list__search w-100"
-          inputMode="search"
-          variant="outlined"
-          size="small"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </div>
+    <Sidebar position="left" className="chat-list" scrollable={true}>
+      <Search placeholder="Search..." />
+      <ConversationList className="chat-list__container">
+        {chatUsers.map((user, idx) => {
+          let userAvatar = DefaultUser;
 
-      <div className="chat-list__container">
-        {chatUsers.map((user) => (
-          <div className="list-user p-3">
-            <Avatar
-              alt="user-avatar"
-              src={user.avatar}
-              sx={{ width: 48, height: 48 }}
+          checkIfImageExists(user.avatar, (exists) => {
+            if (exists) userAvatar = user.avatar;
+          });
+
+          return (
+            <Conversation
+              key={user.userId}
+              name={user.username}
+              info=""
+              className="list-user"
+              active={idx === 0}
             >
-              {user.username.slice(0, 2)}
-            </Avatar>
-            <div className="list-user__info">
-              <div className="info-username">{user.username}</div>
-              <div className="info-latestMsg">I am chill bro</div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+              <Avatar
+                src={userAvatar}
+                name={user.username}
+                status="available"
+              />
+            </Conversation>
+          );
+        })}
+      </ConversationList>
+    </Sidebar>
   );
 };
 
--- src/pages/Chat/template/ChatMainView.tsx
+++ src/pages/Chat/template/ChatMainView.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import ChatList from "./ChatList";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  Avatar,
+  ConversationHeader,
+  TypingIndicator,
+  VoiceCallButton,
+  VideoCallButton,
+  EllipsisButton,
+  MessageSeparator,
+} from "@chatscope/chat-ui-kit-react";
 
 type Props = {
   chatUsers: any[];
@@ -8,21 +22,159 @@ type Props = {
 
 const ChatMainView = (props: Props) => {
   const { chatUsers } = props;
+  const [messageInputValue, setMessageInputValue] = useState("");
 
   return (
-    <div className="chat h-100">
-      <div className="container-fluid p-0 h-100">
-        {chatUsers && (
-          <div className="row m-0 h-100">
-            <div className="col-1 col-md-3 h-100 p-0">
-              <ChatList chatUsers={chatUsers} />
-            </div>
-            <div className="col p-0">
-              <Outlet />
-            </div>
-          </div>
-        )}
-      </div>
+    <div style={{ position: "relative", height: "500px" }}>
+      <MainContainer responsive>
+        <ChatList chatUsers={chatUsers} />
+
+        <ChatContainer>
+          <ConversationHeader>
+            <ConversationHeader.Back />
+            <Avatar src="" name="Zoe" />
+            <ConversationHeader.Content
+              userName="Zoe"
+              info="Active 10 mins ago"
+            />
+            <ConversationHeader.Actions>
+              <VoiceCallButton />
+              <VideoCallButton />
+              <EllipsisButton orientation="vertical" />
+            </ConversationHeader.Actions>
+          </ConversationHeader>
+          <MessageList
+            typingIndicator={<TypingIndicator content="Zoe is typing" />}
+          >
+            <MessageSeparator />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "single",
+              }}
+            >
+              <Avatar src="" name="Zoe" />
+            </Message>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "single",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "first",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "normal",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "normal",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "last",
+              }}
+            >
+              <Avatar src="" name="Zoe" />
+            </Message>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "first",
+              }}
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "normal",
+              }}
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "normal",
+              }}
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "last",
+              }}
+            />
+
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "first",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "last",
+              }}
+            >
+              <Avatar src="" name="Zoe" />
+            </Message>
+          </MessageList>
+          <MessageInput
+            placeholder="Type message here"
+            value={messageInputValue}
+            onChange={(val) => setMessageInputValue(val)}
+          />
+        </ChatContainer>
+      </MainContainer>
     </div>
   );
 };
